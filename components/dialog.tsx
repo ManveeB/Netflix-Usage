f8ea829ab1510afa6f997c07208b833e0c3475eb
ManveeB <bhansalimanvee99@gmail.com>
2026-03-04 22:10:18 +0100

    Update import path for utils in dialog.tsx

--- components/dialog.tsx
+++ components/dialog.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import * as DialogPrimitive from "@radix-ui/react-dialog"
 import { X } from "lucide-react"
 
-import { cn } from "@/lib/utils"
+import { cn } from "../lib/utils.ts" // Changed @/lib/ to ../lib/ and added .ts
 
 const Dialog = DialogPrimitive.Root
 

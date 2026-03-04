8842cd20ee87dcfa0cf35c143c1e2729a0500fcb
ManveeB <bhansalimanvee99@gmail.com>
2026-03-04 21:55:15 +0100

    Refactor import paths in toaster.tsx
    Updated import paths for useToast and Toast components to use relative paths.

--- components/toaster.tsx
+++ components/toaster.tsx
@@ -1,4 +1,4 @@
-import { useToast } from "@/hooks/use-toast"
+import { useToast } from "../hooks/use-toast.ts" // Changed @/ to ../
 import {
   Toast,
   ToastClose,
@@ -6,28 +6,4 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "@/components/ui/toast"
-
-export function Toaster() {
-  const { toasts } = useToast()
-
-  return (
-    <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
-        return (
-          <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
-            </div>
-            {action}
-            <ToastClose />
-          </Toast>
-        )
-      })}
-      <ToastViewport />
-    </ToastProvider>
-  )
-}
+} from "./toast.tsx" // Changed @/components/ui/ to ./ since toast is in the same folder

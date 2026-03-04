7f1a72e5e1506a41fbd1577d8197bdbcfa5bfb2b
ManveeB <bhansalimanvee99@gmail.com>
2026-03-04 22:43:10 +0100

    Implement Toaster component with toast rendering

--- components/toaster.tsx
+++ components/toaster.tsx
@@ -7,3 +7,26 @@ import {
   ToastTitle,
   ToastViewport,
 } from "./toast.tsx" // Changed @/components/ui/ to ./ since toast is in the same folder
+export function Toaster() {
+  const { toasts } = useToast()
+
+  return (
+    <ToastProvider>
+      {toasts.map(function ({ id, title, description, action, ...props }) {
+        return (
+          <Toast key={id} {...props}>
+            <div className="grid gap-1">
+              {title && <ToastTitle>{title}</ToastTitle>}
+              {description && (
+                <ToastDescription>{description}</ToastDescription>
+              )}
+            </div>
+            {action}
+            <ToastClose />
+          </Toast>
+        )
+      })}
+      <ToastViewport />
+    </ToastProvider>
+  )
+}

6b1f2ee57fe1b5a44f1f7d1dfb6284bc721734ae
ManveeB <bhansalimanvee99@gmail.com>
2026-03-04 22:18:17 +0100

    Update not-found.tsx

--- pages/not-found.tsx
+++ pages/not-found.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "../components/card.tsx"; // Removed /ui/ and added .tsx
 import { AlertCircle } from "lucide-react";
 
 export default function NotFound() {

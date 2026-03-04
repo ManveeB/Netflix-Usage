16951dfe5c6097db34c5347959da6074d409c0f6
ManveeB <bhansalimanvee99@gmail.com>
2026-03-04 21:43:07 +0100

    Change Toaster import path to use .txt extension
    Updated import path for Toaster component to use .txt extension.

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "./components/ui/toaster"; // Changed @/ to ./
+import { Toaster } from "./components/ui/toaster.txt"; // Changed @/ to ./
 import { TooltipProvider } from "./components/ui/tooltip"; // Changed @/ to ./
 import Dashboard from "./pages/Dashboard"; // Changed @/ to ./
 import NotFound from "./pages/not-found"; // Changed @/ to ./

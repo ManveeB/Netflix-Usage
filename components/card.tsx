42044c7bcf5cbaedca0a7504cf28dffe27d3cde0
ManveeB <bhansalimanvee99@gmail.com>
2026-03-04 22:07:59 +0100

    Update import path for utils in card.tsx

--- components/card.tsx
+++ components/card.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 
-import { cn } from "@/lib/utils"
+import { cn } from "../lib/utils.ts" // Changed @/lib/ to ../lib/ and added .ts
 
 const Card = React.forwardRef<
   HTMLDivElement,

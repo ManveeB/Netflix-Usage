38a3dd0ae8ad38312cbf12a9e234ae26d0c955aa
Manvee Madanlal Bhansali <bhansalimanvee99@gmail.com>
2026-03-05 09:51:20 +0100

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,14 +1,8 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-import pg from "pg";
+// server/db.ts
 import * as schema from "../shared/schema.ts";
 
-const { Pool } = pg;
-
-if (!process.env.DATABASE_URL) {
-  throw new Error(
-    "DATABASE_URL must be set. Did you forget to provision a database?",
-  );
-}
-
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-export const db = drizzle(pool, { schema });
+// We are exporting an empty object or null because the app 
+// will now use MemStorage instead of Drizzle.
+export const db = {} as any;
+export const pool = {} as any;
+export { schema };
